e91fb8ea977fa531f2363001a026b710d4bffaa5
EricB-59 <151823587+EricB-59@users.noreply.github.com>
2024-10-18 16:36:46 +0200

    cs-fixer_fix-findName

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -1,16 +1,14 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Nurses;
-use App\Repository\NursesRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Entity\Nurses;
 
 #[Route('/nurse', name: 'app_nurse')]
 class NurseController extends AbstractController
@@ -31,6 +29,7 @@ class NurseController extends AbstractController
                 'password' => $nurse->getPassword(),
             ];
         }
+
         // Retorna los datos como una respuesta JSON
         return new JsonResponse($nursesArray, Response::HTTP_OK);
     }
@@ -41,18 +40,18 @@ class NurseController extends AbstractController
     {
         $nurses_repo = $entityManager->getRepository(Nurses::class);
         $nurses = $nurses_repo->findAll();
-        
+
         $firstName = $request->request->get('first_name');
         $password = $request->request->get('password');
 
         foreach ($nurses as $nurse) {
-            if  ($nurse->getFirstName() === $firstName) {
-                if($nurse->getPassword() === $password) {
+            if ($nurse->getFirstName() === $firstName) {
+                if ($nurse->getPassword() === $password) {
                     return $this->json(true, Response::HTTP_OK);
                 }
             }
-            
         }
+
         return $this->json(false, Response::HTTP_NOT_FOUND);
     }
 
@@ -62,21 +61,22 @@ class NurseController extends AbstractController
     {
         $nameNurse = $peticionNurse->query->get('first_name');
         $nurseRepository = $entityManager->getRepository(Nurses::class);
-        $nurses = $nurseRepository->findBy(['first_name'=> $nameNurse]);
+        $nurses = $nurseRepository->findBy(['first_name' => $nameNurse]);
         $nurseArray = [];
         if (!empty($nurses)) {
-          foreach ($nurses as $nurse) {
-            $nurseArray[] = [
-                'id'=>$nurse->getId(),
-                'first_name'=>$nurse->getFirstName(),
-                'last_name'=>$nurse->getLastName(),
-                'email' => $nurse->getEmail(),
-            ];
-            return new JsonResponse($nurseArray, Response::HTTP_OK);
-          }
-        } else {
-            // Si no se encuentra el nombre, retornar 404 con un mensaje
-            return new JsonResponse(['message' => 'El enfermero con ese nombre no existe.'], Response::HTTP_NOT_FOUND);
+            foreach ($nurses as $nurse) {
+                $nurseArray[] = [
+                    'id' => $nurse->getId(),
+                    'first_name' => $nurse->getFirstName(),
+                    'last_name' => $nurse->getLastName(),
+                    'email' => $nurse->getEmail(),
+                ];
+
+                return new JsonResponse($nurseArray, Response::HTTP_OK);
+            }
         }
+
+        // Si no se encuentra el nombre, retornar 404 con un mensaje
+        return new JsonResponse(['message' => 'El enfermero con ese nombre no existe.'], Response::HTTP_NOT_FOUND);
     }
 }
